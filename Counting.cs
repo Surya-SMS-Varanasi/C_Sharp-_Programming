9b4e6377ffc0ccbed04b3d5e87ee1643b491a110
Surya Pratap Singh <suryaprataps2024@gmail.com>
2026-03-11 17:11:10 +0530

    Add C# program to count from 1 to 50
    Implemented a C# program to print numbers from 1 to 50.

---
+++ Counting.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question5
+{
+    internal class Program
+    {
+        // Write a C# program to print numbers from 1 to 50 using for loop.
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 50; i++)
+                Console.WriteLine(i);
+        }
+    }
+}
